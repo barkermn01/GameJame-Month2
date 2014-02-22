8b114920618174faba92d1a8c933e46b5c80765d
JSterling8 <u1276062@unimail.hud.ac.uk>
2014-02-22 19:12:24 +0000

    Zombies fixed
    Going off screen removes 1 point from score.
    Sorting Layer set to top.

--- Assets/ClownSpawner.cs
+++ Assets/ClownSpawner.cs
@@ -16,15 +16,14 @@ public class ClownSpawner : MonoBehaviour {
 	void Update () {
 		timeCount += Time.deltaTime;
 		if(timeCount > spawnTime){
-			if(spawnTime > 0.2f){spawnTime -= 0.1f;}
+			if(spawnTime > 0.5f){spawnTime -= 0.1f;}
 			timeCount = 0.0f;
-			bool startLeft = (Random.Range(0,2) == 0)? false:true;
 			Vector3 StartLoc = new Vector3(
-				(Random.Range(1,2) == 0)? -10.0f:10.0f,
+				(Random.Range(0,2) == 0)? -10.0f:10.0f,
 				Random.Range(-3.9f, 1.4f),
 				0
 				);
-			Instantiate(clown, StartLoc, Quaternion.identity);
+			Instantiate((Random.Range(0,2) == 0) ? clown : zombieClown, StartLoc, Quaternion.identity);
 		}
 	}
 }
--- Assets/Scripts/ClownMovement.cs
+++ Assets/Scripts/ClownMovement.cs
@@ -10,6 +10,8 @@ public class ClownMovement : MonoBehaviour {
 	public float speed;
 	public bool isZombie;
 
+	ScoreManager score;
+
 	// Use this for initialization
 	void Start () {
 		if(transform.localPosition.x < 0){
@@ -18,16 +20,31 @@ public class ClownMovement : MonoBehaviour {
 			moving = Movement.Left;
 		}
 
+		score = (ScoreManager)GameObject.FindGameObjectWithTag("background").GetComponent(typeof(ScoreManager));
+
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		transform.localPosition += new Vector3( this.moving == Movement.Left? -speed:speed, 0,0);
+
+		if (transform.localPosition.x < -10.01f && moving == Movement.Left){
+			if (isZombie){
+				score.Score -= 1;
+			}
+			Destroy (gameObject);
+		}
+		else if (transform.localPosition.x > 10.01f && moving == Movement.Right){
+			if (isZombie){
+				score.Score -= 1;
+			}
+			Destroy (gameObject);
+		}
 	}
 
 	void OnMouseUp(){
 		Destroy(gameObject);
-		ScoreManager score = (ScoreManager)GameObject.FindGameObjectWithTag("background").GetComponent(typeof(ScoreManager));
+
 
 		if(isZombie){
 			score.Score+=2;

7439471f73d702cb667da4045b734f7c3a86e093
barkermn01 <welfordmartin@gmail.com>
2014-02-22 18:55:58 +0000

    Added Spawner
    Added Spawwner

--- Assets/ClownSpawner.cs
+++ Assets/ClownSpawner.cs
@@ -4,25 +4,26 @@ using System.Collections;
 public class ClownSpawner : MonoBehaviour {
 	public GameObject clown;
 	public GameObject zombieClown;
-
-	public float spawnTime = 4;
+	
+	public float spawnTime = 2;
 	private float timeCount = 0;
 	// Use this for initialization
 	void Start () {
-
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		timeCount += Time.deltaTime;
 		if(timeCount > spawnTime){
+			if(spawnTime > 0.2f){spawnTime -= 0.1f;}
 			timeCount = 0.0f;
-			bool startLeft = (Random.Range(0,1) == 0)? false:true;
+			bool startLeft = (Random.Range(0,2) == 0)? false:true;
 			Vector3 StartLoc = new Vector3(
-				(Random.Range(0,1) == 0)? -10.0f:10.0f,
-				Random.Range(1.4f, 3.9f),
+				(Random.Range(1,2) == 0)? -10.0f:10.0f,
+				Random.Range(-3.9f, 1.4f),
 				0
-			);
+				);
 			Instantiate(clown, StartLoc, Quaternion.identity);
 		}
 	}

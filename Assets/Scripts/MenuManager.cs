94e8f764e780ef495357d472b357b32850fefecb
JSterling8 <u1276062@unimail.hud.ac.uk>
2014-02-22 20:05:48 +0000

    Finished
    Finished

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+	public Texture background;
+	public Texture start;
+	public Texture exit;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnGUI(){
+		GUI.DrawTexture(new Rect(0,0, background.width/1.4f, background.height/2), background);
+		GUI.DrawTexture(new Rect((Screen.width-start.width)/2, 50, start.width, start.height), start);
+		GUI.DrawTexture(new Rect((Screen.width-exit.width)/2, 160, exit.width, exit.height), exit);
+	}
+
+	void OnMouseUp(){
+		if(
+			Input.mousePosition.x > (Screen.width-start.width)/2 &&
+			Input.mousePosition.x < ((Screen.width-start.width)/2)+start.width &&
+			Input.mousePosition.y > 50 &&
+			Input.mousePosition.y < 50+start.height
+		){
+			Application.LoadLevel(1);
+		}
+
+		if(
+			Input.mousePosition.x > (Screen.width-exit.width)/2 &&
+			Input.mousePosition.x < ((Screen.width-exit.width)/2)+exit.width &&
+			Input.mousePosition.y > 160 &&
+			Input.mousePosition.y < 160+exit.height
+			){
+			Application.Quit();
+		}
+	}
+}
--- Assets/Scripts/TimeTicker.cs
+++ Assets/Scripts/TimeTicker.cs
@@ -13,7 +13,12 @@ public class TimeTicker : MonoBehaviour {
 			transform.Rotate(Vector3.forward, -6f);
 			Debug.Log (transform.localRotation.eulerAngles);
 			if (transform.localRotation.eulerAngles.z <= 0){
-				yield return new WaitForSeconds(200f);
+				if(Application.loadedLevel == 1){
+					Application.LoadLevel (2);
+				}else{
+					Application.LoadLevel(0);
+				}
+				//yield return new WaitForSeconds(200f);
 			}
 
 			yield return new WaitForSeconds(0.5f);
